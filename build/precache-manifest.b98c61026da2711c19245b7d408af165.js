51c5073803036ec2cdc25653126a6a498dbfa431
vijaykolar <vijayikolar@gmail.com>
2019-03-14 18:21:00 +0530

    Added build

---
+++ build/precache-manifest.b98c61026da2711c19245b7d408af165.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b5272467208c15b073d2",
+    "url": "/static/js/main.b5272467.chunk.js"
+  },
+  {
+    "revision": "0accef2a07fc0896f9a6",
+    "url": "/static/js/2.0accef2a.chunk.js"
+  },
+  {
+    "revision": "b5272467208c15b073d2",
+    "url": "/static/css/main.47c252ec.chunk.css"
+  },
+  {
+    "revision": "adc3f7989693fbdf64f5a4e72b8cee6e",
+    "url": "/index.html"
+  }
+];
